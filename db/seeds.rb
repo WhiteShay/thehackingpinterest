05cc2662e55c7e7a9f3f5d0ef05c6538f90ddfe0
hnassri <46187243+hnassri@users.noreply.github.com>
2019-01-30 22:17:18 +0100

    ajout coms

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,13 @@
 require 'faker'
+# On crée 10 utilisateurs ayant des noms tirés de Game of Thrones
 10.times do
   user = User.create!(name: Faker::GameOfThrones.character)
 end
-
+# On crée 10 pins avec un url auquel onn associe l'id de l'utilisateur
 10.times do 
   pin = Pin.create!(url: Faker::Internet.url, user_id: rand(1..11))
 end
-
+# On crée un commentaire auquel on associe l'id de l'utilisaeur mais aussi l'id du pin
 10.times do
   comment = Comment.create!(content:Faker::StrangerThings.quote,user_id:rand(1..11) , pin_id:rand(1..11))
 end
