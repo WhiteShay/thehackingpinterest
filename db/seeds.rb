a75451fba69c97dcc40e5a124a0ec29be388e66c
 NoraBourouis <bourouisnora@gmail.com>
2019-01-30 19:39:10 +0100

    manque les commentaires

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+10.times do
+  user = User.create!(name: Faker::GameOfThrones.character)
+end
+
+10.times do 
+  pin = Pin.create!(url: Faker::Internet.url, user_id: rand(1..11))
+end
+
+10.times do
+  comment = Comment.create!(content:Faker::StrangerThings.quote,user_id:rand(1..11) , pin_id:rand(1..11))
+end
